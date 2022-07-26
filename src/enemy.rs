52255e78037867935facb230d75722d9444acf84
Rob Parrett <robparrett@gmail.com>
2022-07-26 07:01:47 -0700

    Ramp up the spawn rate as time passes

--- src/enemy.rs
+++ src/enemy.rs
@@ -1,8 +1,11 @@
+use std::time::Duration;
+
 use bevy::prelude::*;
 use rand::{thread_rng, Rng};
 
-use crate::{Health, Player, Velocity};
+use crate::{Health, MaxVelocity, Player, Velocity};
 
+struct RampUpTimer(Timer);
 struct SpawnTimer(Timer);
 #[derive(Component)]
 pub struct Enemy;
@@ -11,8 +14,10 @@ pub struct EnemyPlugin;
 impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(SpawnTimer(Timer::from_seconds(5., true)))
+            .insert_resource(RampUpTimer(Timer::from_seconds(30., true)))
             .add_system(spawn_enemy)
-            .add_system(move_enemy);
+            .add_system(move_enemy)
+            .add_system(ramp_up);
     }
 }
 
@@ -50,20 +55,32 @@ fn spawn_enemy(
         })
         .insert(Enemy)
         .insert(Health {
-            current: 5.,
-            max: 5.,
+            current: 1.,
+            max: 1.,
         })
+        .insert(MaxVelocity(30.))
         .insert(Velocity::default());
 }
 
 fn move_enemy(
     player_query: Query<&Transform, With<Player>>,
-    mut enemy_query: Query<(&mut Velocity, &Transform), With<Enemy>>,
+    mut enemy_query: Query<(&mut Velocity, &MaxVelocity, &Transform), With<Enemy>>,
 ) {
     let player = player_query.single();
 
-    for (mut velocity, transform) in enemy_query.iter_mut() {
+    for (mut velocity, max_velocity, transform) in enemy_query.iter_mut() {
         let diff = player.translation.truncate() - transform.translation.truncate();
-        velocity.0 = diff.normalize() * 30.;
+
+        velocity.0 = diff.normalize() * max_velocity.0;
     }
+}
+
+fn ramp_up(time: Res<Time>, mut spawn: ResMut<SpawnTimer>, mut ramp: ResMut<RampUpTimer>) {
+    ramp.0.tick(time.delta());
+    if !ramp.0.just_finished() {
+        return;
+    }
+
+    let new = (spawn.0.duration().as_secs_f32() / 2.).max(0.4);
+    spawn.0.set_duration(Duration::from_secs_f32(new));
 }
